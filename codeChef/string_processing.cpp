e7c9b8265dc2b9a1f4bfe7ad82c129b013e714bc
radialHuman <52597852+radialHuman@users.noreply.github.com>
2019-09-07 17:53:54 +0530

    Add files via upload

---
+++ codeChef/string_processing.cpp
@@ -0,0 +1,39 @@
+// Given an alphanumeric string made up of digits and lower case Latin characters only, find the sum of all the digit characters in the string.
+
+// Input
+// The first line of the input contains an integer T denoting the number of test cases. Then T test cases follow.
+// Each test case is described with a single line containing a string S, the alphanumeric string.
+// Output
+// For each test case, output a single line containing the sum of all the digit characters in that string.
+// Constraints
+// 1 ≤ T ≤ 1000
+// 1 ≤ |S| ≤ 1000, where |S| is the length of the string S.
+// Example
+// Input:
+// 1
+// ab1231da
+
+// Output:
+// 7
+// Explanation
+// The digits in this string are 1, 2, 3 and 1. Hence, the sum of all of them is 7.
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    int trails;
+    std::string input;
+    std::cin >> trails;
+    for(auto i= 0; i<trails; i++)
+    {
+        std::cin >> input;
+        int c = 0;
+        for(auto j: input)
+            if(isdigit(j))
+                c += static_cast<int>(j)-'0';
+        std::cout << c <<std::endl;
+    }
+}
+
